5fd944c34b70a4f60ce7b4642761507dd62df548
Nathan Schulzke <nschulzke@gmail.com>
2023-05-07 16:54:41 -0600

    Update example to show delegation

--- examples/example.ts
+++ examples/example.ts
@@ -1,18 +1,13 @@
-export default async function ({files, console}: Myco) {
-    const path = "./log.txt";
-    try {
-        const rToken = await files.requestRead(path);
-        const contents = await rToken.read();
-        console.log("Read from a file", contents);
-    } catch (err) {
-        console.error("Unable to read file", path, err);
-    }
+let console: Myco.Console;
 
-    const rwToken = await files.requestReadWrite(path);
-    await rwToken.write("I can write to a file.");
-    const contents = await rwToken.read();
-    console.log("Read from a file", path, "contents:", contents);
-    console.log("Removing file", path);
-    await rwToken.remove();
-    console.log("File removed");
+export default async function ({files, console: _console}: Myco) {
+    console = _console;
+    const directory = await files.requestReadWriteDir("./data");
+    await childFunction(directory);
+}
+
+async function childFunction(token: Myco.Files.ReadWriteDirToken) {
+    const contents = await token.read("../build.rs");
+    const transform = contents.replace("a", "b");
+    await token.write("./log.txt", transform);
 }
