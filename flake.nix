cc011ec2871940bdaf21b03cbf2b37e299a75f75
Nathan Schulzke <nathan@jetmail.org>
2025-06-07 20:35:52 -0500

    Switch shell.nix for flake.nix

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Development environment for Rust project with v8 and openssl dependencies";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # Rust toolchain
+            rustc
+            cargo
+            rustfmt
+            clippy
+
+            # # Build dependencies for v8 and openssl
+            pkg-config
+            openssl
+            openssl.dev
+          ];
+
+          # Environment variables needed for linking
+          shellHook = ''
+            export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
+            export OPENSSL_DIR="${pkgs.openssl.dev}"
+            export OPENSSL_LIB_DIR="${pkgs.openssl.out}/lib"
+            export OPENSSL_INCLUDE_DIR="${pkgs.openssl.dev}/include"
+          '';
+        };
+      });
+} 
